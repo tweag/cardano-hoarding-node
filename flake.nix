0893f8f0c76fa6417f7d6fd4287c3cab51c80963
Christian Georgii <cgeorgii@gmail.com>
2025-10-27 22:44:42 +0100

    chore: add cardano-api to deps

--- flake.nix
+++ flake.nix
@@ -4,11 +4,9 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.iog.io"
-      "https://cache.zw3rk.com"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcJQY="
     ];
     allow-import-from-derivation = true;
   };
--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,6 +1,10 @@
 { inputs, system }:
 import inputs.haskell-nix.inputs.nixpkgs {
   inherit system;
-  overlays = [ inputs.haskell-nix.overlay ];
+  overlays = [
+    inputs.iohk-nix.overlays.crypto
+    inputs.iohk-nix.overlays.haskell-nix-crypto
+    inputs.haskell-nix.overlay
+  ];
   inherit (inputs.haskell-nix) config;
 }
