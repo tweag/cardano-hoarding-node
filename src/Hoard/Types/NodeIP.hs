ed4594e9eb6f5148ac5b687297c70758700cb4fc
Victor Nascimento Bakke <victor.bakke@tweag.io>
2025-11-06 22:27:49 +0100

    refactor: derive NodeIP DB and JSON instances from newtypes

--- src/Hoard/Types/NodeIP.hs
+++ src/Hoard/Types/NodeIP.hs
@@ -2,37 +2,14 @@ module Hoard.Types.NodeIP
     ( NodeIP (..)
     ) where
 
-import Data.Aeson (FromJSON (..), ToJSON (..), Value (String), withText)
+import Data.Aeson (FromJSON (..), ToJSON (..))
 import Data.IP (IP)
-import Data.Text (Text)
-import Data.Text qualified as T
-import GHC.Generics (Generic)
-import Rel8 (DBEq, DBType (..), parseTypeInformation)
-import Text.Read (readMaybe)
+import Hoard.Types.JsonReadShow (JsonReadShow (..))
+import Rel8 (DBEq, DBType (..), ReadShow (..))
 
 
 newtype NodeIP = NodeIP {getNodeIP :: IP}
     deriving stock (Eq, Ord, Generic)
     deriving (Show, Read) via (IP)
-
-
-instance FromJSON NodeIP where
-    parseJSON = withText "NodeIP" $ \t ->
-        case readMaybe $ T.unpack t of
-            Just ip -> pure $ NodeIP ip
-            Nothing -> fail "Not a valid IP string"
-
-
-instance ToJSON NodeIP where
-    toJSON = String . T.pack . show . getNodeIP
-
-
-instance DBType NodeIP where
-    typeInformation =
-        parseTypeInformation
-            (maybe (Left "Not a valid IP string") (Right . NodeIP) . readMaybe . T.unpack)
-            (T.pack . show . getNodeIP)
-            (typeInformation @Text)
-
-
-instance DBEq NodeIP
+    deriving (DBType, DBEq) via ReadShow NodeIP
+    deriving (FromJSON, ToJSON) via JsonReadShow NodeIP
