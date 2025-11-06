339cb147076469c52fa51e673c2bef805e35427c
Victor Nascimento Bakke <victor.bakke@tweag.io>
2025-11-06 12:54:17 +0000

    fix: incorrect haskell-language-server for the version of GHC we use (#44)

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   project,
   gitHooks,
   tools,
@@ -7,7 +8,15 @@ let
   inherit (project.args) compiler-nix-name;
 
   # System tools not tied to GHC version
-  systemTools = builtins.attrValues tools;
+  systemTools =
+    builtins.attrValues tools
+    ++ (with pkgs; [
+      nixfmt-rfc-style
+      postgresql
+      sqitchPg
+      sops
+      age
+    ]);
 in
 project.shellFor {
   name = "hoard-shell-${compiler-nix-name}";
@@ -19,6 +28,14 @@ project.shellFor {
   withHoogle = true;
 
   buildInputs = systemTools;
+
+  tools = {
+    cabal = "latest";
+    haskell-language-server = "latest";
+    ghcid = "latest";
+    tasty-discover = "latest";
+    weeder = "latest";
+  };
 
   shellHook = ''
     # Git hooks integration
