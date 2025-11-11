b7c0b94c0421d820ffec7e0b7ba61d20bf512367
Christian Georgii <cgeorgii@gmail.com>
2025-11-11 17:53:57 +0100

    refactor: deduplicate Environment parsing logic
    Use inverseMap with environmentName to eliminate redundant pattern matching
    in IsString and FromJSON instances for the Environment type.

--- src/Hoard/Types/Environment.hs
+++ src/Hoard/Types/Environment.hs
@@ -21,31 +21,20 @@ data Environment
 
 
 instance IsString Environment where
-    fromString = \case
-        "dev" -> Dev
-        "staging" -> Staging
-        "prod" -> Prod
-        "ci" -> CI
-        s -> error $ "Invalid environment: " <> cs s
+    fromString s = case parseEnvironment (cs s) of
+        Just env -> env
+        Nothing -> error $ "Invalid environment: " <> cs s
 
 
 instance FromJSON Environment where
-    parseJSON = withText "Environment" $ \case
-        "dev" -> pure Dev
-        "staging" -> pure Staging
-        "prod" -> pure Prod
-        "ci" -> pure CI
-        other -> fail $ "Invalid environment: " <> T.unpack other
+    parseJSON = withText "Environment" $ \x -> case parseEnvironment x of
+        Just env -> pure env
+        Nothing -> fail $ "Invalid environment: " <> T.unpack x
 
 
--- | Parse environment from a string (case-insensitive)
+-- | Parse environment from a string
 parseEnvironment :: Text -> Maybe Environment
-parseEnvironment text = case T.toLower text of
-    "dev" -> Just Dev
-    "staging" -> Just Staging
-    "prod" -> Just Prod
-    "ci" -> Just CI
-    _ -> Nothing
+parseEnvironment = inverseMap environmentName . T.toLower
 
 
 -- | Get the lowercase name of an environment
