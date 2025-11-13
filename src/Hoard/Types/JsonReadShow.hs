c9aa0002f0b85d16c72cc279fd6a1b5365d81b73
Victor Nascimento Bakke <victor.bakke@tweag.io>
2025-11-13 08:52:05 +0100

    refactor: derive NodeIP DB and JSON instances from newtypes (#63)

---
+++ src/Hoard/Types/JsonReadShow.hs
@@ -0,0 +1,24 @@
+module Hoard.Types.JsonReadShow (JsonReadShow (..)) where
+
+import Data.Aeson (FromJSON (..), ToJSON (..), Value (..), withText)
+import Data.Typeable (typeRep)
+
+
+newtype JsonReadShow a = JsonReadShow {getJsonReadShow :: a}
+
+
+instance forall a. (Typeable a, Read a) => FromJSON (JsonReadShow a) where
+    parseJSON =
+        let
+            typeName = show $ typeRep $ Proxy @a
+        in
+            withText typeName $
+                maybe
+                    (fail $ "Failed to read " <> typeName)
+                    (pure . JsonReadShow)
+                    . readMaybe
+                    . show
+
+
+instance (Show a) => ToJSON (JsonReadShow a) where
+    toJSON = String . show . getJsonReadShow
