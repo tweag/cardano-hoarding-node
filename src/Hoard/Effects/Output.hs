765d79e61e0ffa36e3000ba5d852d0c1b582b9d1
Victor Nascimento Bakke <victor.bakke@tweag.io>
2025-11-24 16:21:48 +0100

    feat: add block fetch implementation

---
+++ src/Hoard/Effects/Output.hs
@@ -0,0 +1,58 @@
+module Hoard.Effects.Output
+    ( Output (..)
+    , output
+    , runOutputEff
+    , runOutputDiscard
+    , runOutputChan
+    , runOutputAsPub
+    , runOutputList
+    )
+where
+
+import Prelude hiding (modify, runState)
+
+import Control.Concurrent.Chan.Unagi (InChan, writeChan)
+import Data.Dynamic qualified as Dyn
+import Effectful (Eff, Effect, IOE, (:>))
+import Effectful.Dispatch.Dynamic (interpret_, reinterpret_)
+import Effectful.State.Static.Local (modify, runState)
+import Effectful.TH (makeEffect)
+import Hoard.Effects.Pub (Pub, publish)
+
+
+-- | Output something to whomever cares in a manner that is of no concern for
+-- the logic producing the outputted value.
+data Output a :: Effect where
+    Output :: a -> Output a m ()
+
+
+makeEffect ''Output
+
+
+-- | Run an Output effect by discarding all outputted values.
+runOutputDiscard :: Eff (Output a : es) x -> Eff es x
+runOutputDiscard = interpret_ $ \(Output _) -> pure ()
+
+
+-- | Run an Output effect using a given Eff-producing function.
+runOutputEff :: (a -> Eff es ()) -> Eff (Output a : es) x -> Eff es x
+runOutputEff eff = interpret_ $ \(Output x) -> eff x
+
+
+-- | Run an Output effect by writing to an `InChan`.
+runOutputChan :: (IOE :> es) => InChan a -> Eff (Output a : es) x -> Eff es x
+runOutputChan inChan = runOutputEff $ liftIO . writeChan inChan
+
+
+-- | Run an Output by publishing values with the `Pub` effect.
+runOutputAsPub :: (Typeable a, Pub :> es) => Eff (Output a : es) x -> Eff es x
+runOutputAsPub = runOutputEff $ publish . Dyn.toDyn
+
+
+-- | Collect all values from an Output effect into a list.
+runOutputList :: Eff (Output a : es) x -> Eff es (x, [a])
+runOutputList =
+    fmap (second reverse)
+        . reinterpret_
+            (runState [])
+            (\(Output x) -> modify (x :))
