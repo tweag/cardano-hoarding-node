332d5347abcbcd89aeaaf9d9f46ff62bd2c6b540
Philipp Zander <philipp.zander@tweag.io>
2025-12-01 15:31:18 +0100

    use an abstract concurrency effect

--- src/Hoard/Effects/NodeToClient.hs
+++ src/Hoard/Effects/NodeToClient.hs
@@ -29,13 +29,11 @@ import Effectful
     ( Eff
     , Effect
     , IOE
-    , withEffToIO
     , (:>)
     )
 import Effectful.Dispatch.Dynamic (interpret_)
 import Effectful.TH (makeEffect)
-import Hoard.Effects.Conc (concStrat)
-import Ki qualified as Ki
+import Hoard.Effects.Conc (Conc, fork_)
 import Ouroboros.Network.Protocol.LocalStateQuery.Client qualified as Q
 
 
@@ -46,34 +44,33 @@ data NodeToClient :: Effect where
 makeEffect ''NodeToClient
 
 
-runNodeToClient :: (IOE :> es) => LocalNodeConnectInfo -> Eff (NodeToClient : es) a -> Eff es a
+runNodeToClient :: (Conc :> es, IOE :> es) => LocalNodeConnectInfo -> Eff (NodeToClient : es) a -> Eff es a
 runNodeToClient connectionInfo nodeToClient = do
     (immutableTipQueriesIn, immutableTipQueriesOut) <- liftIO newChan
-    withEffToIO concStrat $ \unlift ->
-        liftIO $ Ki.scoped $ \scope -> do
-            _ <- Ki.fork scope $ localNodeClient connectionInfo immutableTipQueriesOut
-            unlift $
-                interpret_
-                    ( \case
-                        ImmutableTip -> liftIO $ do
-                            resultVar <- newEmptyMVar
-                            writeChan immutableTipQueriesIn resultVar
-                            readMVar resultVar
-                    )
-                    nodeToClient
+    _ <- fork_ $ liftIO $ localNodeClient connectionInfo immutableTipQueriesOut
+    interpret_
+        ( \case
+            ImmutableTip -> liftIO $ do
+                resultVar <- newEmptyMVar
+                writeChan immutableTipQueriesIn resultVar
+                readMVar resultVar
+        )
+        nodeToClient
 
 
-localNodeClient :: LocalNodeConnectInfo -> OutChan (MVar ChainPoint) -> IO ()
-localNodeClient connectionInfo immutableTipQueries = do
-    connectToLocalNode
-        connectionInfo
-        LocalNodeClientProtocols
-            { localChainSyncClient = NoLocalChainSyncClient
-            , localStateQueryClient = Just (LocalStateQueryClient queryImmutableTip)
-            , localTxSubmissionClient = Nothing
-            , localTxMonitoringClient = Nothing
-            }
+localNodeClient :: LocalNodeConnectInfo -> OutChan (MVar ChainPoint) -> IO Void
+localNodeClient connectionInfo immutableTipQueries =
+    error "`connectToLocalNode` should run indefinitely because `localStateQueryClient` returns `void`."
+        <$> connectToLocalNode
+            connectionInfo
+            LocalNodeClientProtocols
+                { localChainSyncClient = NoLocalChainSyncClient
+                , localStateQueryClient = Just (LocalStateQueryClient queryImmutableTip)
+                , localTxSubmissionClient = Nothing
+                , localTxMonitoringClient = Nothing
+                }
   where
+    queryImmutableTip :: IO (Q.ClientStIdle block point QueryInMode IO void)
     queryImmutableTip = do
         resultVar <- readChan immutableTipQueries
         pure
