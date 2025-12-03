cfb8c414690e8feb5b660e73498e70f6740db650
Christian Georgii <cgeorgii@gmail.com>
2025-12-03 11:21:42 +0100

    refactor: centralize listener setup in new module
    Created Hoard.Listeners module to centralize all event listener
    initialization, moving runListeners and runServer helper functions
    out of Main.hs.
    
    This simplifies Main.hs by removing inline listener setup code and
    provides a cleaner separation of concerns. The runServer function
    now properly forks itself to run concurrently.

--- app/hoard/Main.hs
+++ app/hoard/Main.hs
@@ -6,21 +6,12 @@ import Options.Applicative qualified as Opt
 
 import Hoard.CLI.Options (Options (..), optsParser)
 import Hoard.Config.Loader (loadConfig)
-import Hoard.Effects (Config (..), ServerConfig (..), runEffectStack)
-import Hoard.Effects.Sub (listen)
-import Hoard.Listeners.ChainSyncEventListener (chainSyncEventListener)
-import Hoard.Listeners.CollectorEventListener (collectorEventListener)
-import Hoard.Listeners.HeaderReceivedListener (headerReceivedListener)
-import Hoard.Listeners.NetworkEventListener (networkEventListener)
-import Hoard.Listeners.PeerSharingEventListener (peerSharingEventListener)
-import Hoard.Listeners.PeersReceivedListener (peersReceivedListener)
+import Hoard.Effects (runEffectStack)
+import Hoard.Listeners (runListeners)
 import Hoard.Types.Environment (Environment (..))
 
-import Effectful (Eff)
-import Hoard.Collector (dispatchDiscoveredNodes)
-import Hoard.Effects (AppEff)
 import Hoard.Effects.Conc qualified as Conc
-import Hoard.Server qualified as Server
+import Hoard.Server (runServer)
 
 
 main :: IO ()
@@ -33,27 +24,7 @@ main = withIOManager $ \ioManager -> do
     putTextLn $ "Loading configuration for environment: " <> show env
     config <- loadConfig ioManager env
 
-    -- Start background threads in the effect stack
-    putTextLn $ "Starting Hoard on " <> config.server.host <> ":" <> show config.server.port <> "..."
     runEffectStack config $ do
         runServer config
         runListeners
         Conc.awaitAll
-
-
-runServer :: (AppEff es) => Config -> Eff es ()
-runServer config = do
-    _ <- Conc.fork $ Server.runServer config
-    pure ()
-
-
-runListeners :: (AppEff es) => Eff es ()
-runListeners = do
-    _ <- Conc.fork $ listen headerReceivedListener
-    _ <- Conc.fork $ listen peersReceivedListener
-    _ <- Conc.fork $ listen dispatchDiscoveredNodes
-    _ <- Conc.fork $ listen networkEventListener
-    _ <- Conc.fork $ listen peerSharingEventListener
-    _ <- Conc.fork $ listen chainSyncEventListener
-    _ <- Conc.fork $ listen collectorEventListener
-    pure ()
--- src/Hoard/Server.hs
+++ src/Hoard/Server.hs
@@ -10,20 +10,21 @@ import Servant
 import Hoard.API (API, server)
 import Hoard.Effects (AppEff, Config (..), ServerConfig (..), runEffectStack)
 
+import Hoard.Effects.Conc qualified as Conc
 import Hoard.Effects.Log qualified as Log
 
 
--- | Run the Servant server with the provided configuration
 runServer :: (AppEff es) => Config -> Eff es ()
 runServer config = do
-    -- Log startup messages
-    let ServerConfig {host = serverHost, port = serverPort} = config.server
-        host = toString serverHost
-        port = fromIntegral serverPort
-    Log.debug $ "Starting Hoard server on " <> toText host <> ":" <> show port
-    Log.debug "Waiting for data..."
+    _ <- Conc.fork $ do
+        -- Log startup messages
+        let ServerConfig {host = serverHost, port = serverPort} = config.server
+            host = toString serverHost
+            port = fromIntegral serverPort
+        Log.debug $ "Starting Hoard server on " <> toText host <> ":" <> show port
 
-    -- Run Warp server (needs liftIO since Warp's runSettings is in IO)
-    let settings = setPort port $ setHost (fromString host) defaultSettings
-        servantApp = hoistServer (Proxy @API) (Handler . runEffectStack config) Hoard.API.server
-    liftIO $ runSettings settings (serve (Proxy @API) servantApp)
+        -- Run Warp server (needs liftIO since Warp's runSettings is in IO)
+        let settings = setPort port $ setHost (fromString host) defaultSettings
+            servantApp = hoistServer (Proxy @API) (Handler . runEffectStack config) Hoard.API.server
+        liftIO $ runSettings settings (serve (Proxy @API) servantApp)
+    pure ()
