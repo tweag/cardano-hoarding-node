8e23dfda3447f3a157dc3f70014daeeac1fd4b2f
Victor Nascimento Bakke <victor.bakke@tweag.io>
2026-02-12 15:29:56 +0100

    refactor: make Conc a static effect (#241)

--- src/Hoard/Effects/Conc.hs
+++ src/Hoard/Effects/Conc.hs
@@ -8,88 +8,92 @@ module Hoard.Effects.Conc
     , awaitAll
     , forkTry
 
-      -- * Scope
-    , Scope
-    , scoped
-
       -- * Interpreters
-    , runConcWithKi
-    , runConcNewScope
-
-      -- * Unlift Strategy
-    , concStrat
+    , runConc
     )
 where
 
-import Prelude hiding (atomically)
-
-import Effectful (Eff, Effect, IOE, Limit (..), Persistence (..), UnliftStrategy (..), withEffToIO, (:>))
-import Effectful.Concurrent.STM (atomically, runConcurrent)
-import Effectful.Dispatch.Dynamic (interpret, localUnliftIO)
-import Effectful.TH (makeEffect)
-
+import Effectful
+    ( Dispatch (..)
+    , DispatchOf
+    , Eff
+    , Effect
+    , IOE
+    , Limit (..)
+    , Persistence (..)
+    , (:>)
+    )
+import Effectful.Dispatch.Static
+    ( SideEffects (..)
+    , StaticRep
+    , concUnliftIO
+    , evalStaticRep
+    , unsafeEff
+    , unsafeSeqUnliftIO
+    )
+import Effectful.Dispatch.Static.Primitive (getEnv)
+import Ki (Scope, Thread)
 import Ki qualified
 
 
-data Conc :: Effect where
-    Fork :: m a -> Conc m (Thread a)
-    Fork_ :: m Void -> Conc m ()
-    Await :: Thread a -> Conc m a
-    AwaitAll :: Conc m ()
-    ForkTry :: (Exception e) => m a -> Conc m (Thread (Either e a))
+data Conc :: Effect
+type instance DispatchOf Conc = Static WithSideEffects
+newtype instance StaticRep Conc = Conc Scope
 
 
-newtype Scope = Scope Ki.Scope
+-- | Fork a computation into a distinct thread and immediately return a
+-- reference to said thread.
+fork :: (Conc :> es) => Eff es a -> Eff es (Thread a)
+fork action = unsafeEff \env -> do
+    Conc scope <- getEnv env
+    concUnliftIO env Persistent (Limited 1) \unlift ->
+        Ki.fork scope $ unlift action
 
 
-newtype Thread a = Thread (Ki.Thread a)
+-- | Fork a computation that blocks indefinitely and immediately return. Since
+-- the computation is expected to block indefinitely, there is no use to
+-- `await` the thread, and thus we don't need a thread reference, so this
+-- function does not return one.
+fork_ :: (Conc :> es) => Eff es Void -> Eff es ()
+fork_ action = unsafeEff \env -> do
+    Conc scope <- getEnv env
+    concUnliftIO env Persistent (Limited 1) \unlift ->
+        Ki.fork_ scope $ unlift action
 
 
-makeEffect ''Conc
+-- | Await a forked thread, blocking the current thread until the awaited
+-- thread terminates.
+await :: (Conc :> es) => Thread a -> Eff es a
+await thread = unsafeEff \env -> do
+    -- We don't use the scope here, but we should use `getStaticRep` here to
+    -- ensure we require `Conc` in the context. Otherwise, this function would
+    -- hide the use of `IO` in `unsafeEff_` a bit too well, and its behavior
+    -- might seem unintuitive.
+    Conc _ <- getEnv env
+    atomically $ Ki.await thread
 
 
-scoped :: (IOE :> es) => (Scope -> Eff es a) -> Eff es a
-scoped action = withEffToIO concStrat $ \unlift ->
-    Ki.scoped $ \scope ->
-        unlift
-            . action
-            $ Scope scope
+-- | Await all forked threads in the current scope.
+awaitAll :: (Conc :> es) => Eff es ()
+awaitAll = unsafeEff \env -> do
+    Conc scope <- getEnv env
+    atomically $ Ki.awaitAll scope
 
 
-runConcWithKi :: (IOE :> es) => Scope -> Eff (Conc : es) a -> Eff es a
-runConcWithKi (Scope scope) = interpret $ \env -> \case
-    Fork action ->
-        localUnliftIO env concStrat $ \unlift ->
-            fmap Thread
-                . liftIO
-                . Ki.fork scope
-                $ unlift action
-    Fork_ action ->
-        localUnliftIO env concStrat $ \unlift ->
-            liftIO
-                . Ki.fork_ scope
-                $ unlift action
-    Await (Thread thread) ->
-        runConcurrent
-            . atomically
-            $ Ki.await thread
-    AwaitAll ->
-        runConcurrent
-            . atomically
-            $ Ki.awaitAll scope
-    ForkTry action ->
-        localUnliftIO env concStrat $ \unlift ->
-            fmap Thread
-                . liftIO
-                . Ki.forkTry scope
-                $ unlift action
+-- | Fork a computation and catch all exceptions in an `Either e a`. This will
+-- not catch:
+-- - Synchronous exceptions that do not match `e`
+-- - Asynchronous exceptions
+forkTry :: forall e a es. (Conc :> es, Exception e) => Eff es a -> Eff es (Thread (Either e a))
+forkTry action = unsafeEff \env -> do
+    Conc scope <- getEnv env
+    concUnliftIO env Persistent (Limited 1) \unlift ->
+        Ki.forkTry scope $ unlift action
 
 
-runConcNewScope :: (IOE :> es) => Eff (Conc : es) a -> Eff es a
-runConcNewScope eff = withEffToIO concStrat $ \unlift ->
-    Ki.scoped $ \scope ->
-        unlift $ runConcWithKi (Scope scope) eff
-
-
-concStrat :: UnliftStrategy
-concStrat = ConcUnlift Persistent Unlimited
+-- | Run a `Conc` effect with `Ki`.
+runConc :: (IOE :> es) => Eff (Conc : es) a -> Eff es a
+runConc eff = do
+    unsafeSeqUnliftIO \unlift ->
+        Ki.scoped \scope ->
+            unlift $ evalStaticRep (Conc scope) eff
