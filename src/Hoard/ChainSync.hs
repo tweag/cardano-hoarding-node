6acbd9c53159c1fa9919c00c28337002adc82b21
Victor Nascimento Bakke <victor.bakke@tweag.io>
2026-02-16 14:10:29 +0100

    refactor: use record-of-functions instead of type class for components (#247)
    * refactor: use record-of-functions instead of type class for components
    
    * refactor: remove redundant tracing events
    
    * fix: withSeqEffToIO unlift called in separate thread

--- src/Hoard/ChainSync.hs
+++ src/Hoard/ChainSync.hs
@@ -1,11 +1,10 @@
-module Hoard.ChainSync (ChainSync (..)) where
+module Hoard.ChainSync (component) where
 
 import Cardano.Api.LedgerState ()
-import Effectful (Eff, (:>))
+import Effectful ((:>))
 
 import Hoard.ChainSync.Events (ChainSyncIntersectionFound, ChainSyncStarted, HeaderReceived, RollBackward, RollForward)
-import Hoard.Component (Component (..), Listener)
-import Hoard.Effects.Conc (Conc)
+import Hoard.Component (Component (..), defaultComponent)
 import Hoard.Effects.HeaderRepo (HeaderRepo)
 import Hoard.Effects.Monitoring.Metrics (Metrics)
 import Hoard.Effects.Monitoring.Tracing (Tracing)
@@ -15,30 +14,26 @@ import Hoard.ChainSync.Listeners qualified as Listeners
 import Hoard.Effects.Publishing qualified as Sub
 
 
-data ChainSync = ChainSync
-
-
-instance Component ChainSync es where
-    type
-        Effects ChainSync es =
-            ( Tracing :> es
-            , Conc :> es
-            , HeaderRepo :> es
-            , Metrics :> es
-            , Sub HeaderReceived :> es
-            , Sub ChainSyncStarted :> es
-            , Sub RollBackward :> es
-            , Sub RollForward :> es
-            , Sub ChainSyncIntersectionFound :> es
-            )
-
-
-    listeners :: (Effects ChainSync es) => Eff es [Listener es]
-    listeners =
-        pure
-            [ Sub.listen Listeners.chainSyncHeaderReceived
-            , Sub.listen Listeners.chainSyncStarted
-            , Sub.listen Listeners.chainSyncRollBackward
-            , Sub.listen Listeners.chainSyncRollForward
-            , Sub.listen Listeners.chainSyncIntersectionFound
-            ]
+component
+    :: ( HeaderRepo :> es
+       , Metrics :> es
+       , Sub ChainSyncIntersectionFound :> es
+       , Sub ChainSyncStarted :> es
+       , Sub HeaderReceived :> es
+       , Sub RollBackward :> es
+       , Sub RollForward :> es
+       , Tracing :> es
+       )
+    => Component es
+component =
+    defaultComponent
+        { name = "ChainSync"
+        , listeners =
+            pure
+                [ Sub.listen Listeners.chainSyncHeaderReceived
+                , Sub.listen Listeners.chainSyncStarted
+                , Sub.listen Listeners.chainSyncRollBackward
+                , Sub.listen Listeners.chainSyncRollForward
+                , Sub.listen Listeners.chainSyncIntersectionFound
+                ]
+        }
--- src/Hoard/Server.hs
+++ src/Hoard/Server.hs
@@ -1,9 +1,9 @@
 module Hoard.Server
-    ( Server (..)
+    ( component
     )
 where
 
-import Effectful (IOE, withSeqEffToIO, (:>))
+import Effectful (IOE, Limit (..), Persistence (..), UnliftStrategy (..), withEffToIO, (:>))
 import Effectful.Exception (try)
 import Effectful.Reader.Static (Reader, ask)
 import Network.Wai.Handler.Warp (defaultSettings, runSettings, setHost, setPort)
@@ -11,48 +11,42 @@ import Servant (Handler (..), hoistServer, serve)
 import Prelude hiding (Reader, ask)
 
 import Hoard.API (API, server)
-import Hoard.Component (Component (..))
+import Hoard.Component (Component (..), defaultComponent)
 import Hoard.Effects.BlockRepo (BlockRepo)
-import Hoard.Effects.Conc (Conc)
 import Hoard.Effects.Log (Log)
 import Hoard.Effects.Monitoring.Metrics (Metrics)
-import Hoard.Effects.Monitoring.Tracing (Tracing)
 import Hoard.Types.Environment (Config (..), Env (..), ServerConfig (..))
 
 import Hoard.Effects.Log qualified as Log
 
 
-data Server = Server
+component
+    :: ( BlockRepo :> es
+       , IOE :> es
+       , Log :> es
+       , Metrics :> es
+       , Reader Env :> es
+       )
+    => Component es
+component =
+    defaultComponent
+        { name = "Server"
+        , start = do
+            env <- ask
+            -- Log startup messages
+            let host = toString env.config.server.host
+                port = fromIntegral env.config.server.port
 
+            Log.debug $ "Starting Hoard server on " <> toText host <> ":" <> show port
 
-instance Component Server es where
-    type
-        Effects Server es =
-            ( BlockRepo :> es
-            , Conc :> es
-            , IOE :> es
-            , Log :> es
-            , Metrics :> es
-            , Reader Env :> es
-            , Tracing :> es
-            )
+            -- Run Warp server (blocking call, but runSystem auto-forks start phases)
+            let settings = setPort port $ setHost (fromString host) defaultSettings
+            servantApp <- withEffToIO (ConcUnlift Persistent Unlimited) \unlift ->
+                pure
+                    $ hoistServer
+                        (Proxy @API)
+                        (Handler . ExceptT . unlift . try)
+                        Hoard.API.server
 
-
-    start = do
-        env <- ask
-        -- Log startup messages
-        let host = toString env.config.server.host
-            port = fromIntegral env.config.server.port
-
-        Log.debug $ "Starting Hoard server on " <> toText host <> ":" <> show port
-
-        -- Run Warp server (blocking call, but runSystem auto-forks start phases)
-        let settings = setPort port $ setHost (fromString host) defaultSettings
-        servantApp <- withSeqEffToIO \unlift ->
-            pure
-                $ hoistServer
-                    (Proxy @API)
-                    (Handler . ExceptT . unlift . try)
-                    Hoard.API.server
-
-        liftIO $ runSettings settings (serve (Proxy @API) servantApp)
+            liftIO $ runSettings settings (serve (Proxy @API) servantApp)
+        }
