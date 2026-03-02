72d6669aff5e86d22f7cd0f8d6acb623f6d6bd97
Victor Nascimento Bakke <victor.bakke@tweag.io>
2026-03-02 19:58:43 +0100

    chore: remove unused modules

--- src/Hoard/Events.hs
+++
@@ -1,7 +0,0 @@
-module Hoard.Events (SomeEvent (..)) where
-
-
--- | Existential wrapper for any event type
--- Allows heterogeneous events to be stored in a single queue
-data SomeEvent where
-    SomeEvent :: (Show e, Typeable e) => e -> SomeEvent
