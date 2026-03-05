543e26f2c7487d28e5a63dba2d195d0317afd0f5
Victor Nascimento Bakke <victor.bakke@tweag.io>
2026-03-05 16:24:58 +0100

    fix: missing encoder and decoder for Byron headers (#304)

--- test/Unit/Hoard/Data/HeaderSpec.hs
+++ test/Unit/Hoard/Data/HeaderSpec.hs
@@ -13,7 +13,10 @@ import Test.Hspec
 import Test.Util.Serialisation.Examples (Examples (..))
 
 import Data.List qualified as List
+import Ouroboros.Consensus.Byron.Ledger.Block qualified as Byron
+import Ouroboros.Consensus.Byron.Ledger.Serialisation qualified as Byron
 import Ouroboros.Consensus.Cardano.Block qualified as O
+import Test.Consensus.Byron.Examples qualified as Byron
 
 import Hoard.Data.Eras (BlockEra (..))
 import Hoard.Data.Header (decodeCardanoHeader, encodeCardanoHeader)
@@ -30,15 +33,24 @@ spec_Header =
 
 blocks :: [(BlockEra, CardanoHeader)]
 blocks =
-    catMaybes
-        $ universe <&> \x -> case x of
-            Byron -> Nothing
-            Allegra -> Just (x, mkHeader O.HeaderAllegra examplesAllegra)
-            Alonzo -> Just (x, mkHeader O.HeaderAlonzo examplesAlonzo)
-            Mary -> Just (x, mkHeader O.HeaderMary examplesMary)
-            Shelley -> Just (x, mkHeader O.HeaderShelley examplesShelley)
-            Babbage -> Just (x, mkHeader O.HeaderBabbage examplesBabbage)
-            Conway -> Just (x, mkHeader O.HeaderConway examplesConway)
-            Dijkstra -> Just (x, mkHeader O.HeaderDijkstra examplesDijkstra)
+    universe <&> \x -> case x of
+        Byron -> (x, O.HeaderByron byronHeader)
+        Allegra -> (x, mkHeader O.HeaderAllegra examplesAllegra)
+        Alonzo -> (x, mkHeader O.HeaderAlonzo examplesAlonzo)
+        Mary -> (x, mkHeader O.HeaderMary examplesMary)
+        Shelley -> (x, mkHeader O.HeaderShelley examplesShelley)
+        Babbage -> (x, mkHeader O.HeaderBabbage examplesBabbage)
+        Conway -> (x, mkHeader O.HeaderConway examplesConway)
+        Dijkstra -> (x, mkHeader O.HeaderDijkstra examplesDijkstra)
   where
     mkHeader mk examples = mk $ snd $ List.head $ examples.exampleHeader
+    -- When deserializing a byron header, we don't know its corresponding
+    -- block's proper size, but the `byronHeaderBlockSizeHint` is used when
+    -- comparing headers using their `Eq` instance, so we just set it to
+    -- `fakeByronBlockSizeHint` for simplicity's sake. The hint is not used for
+    -- any particularly critical logic, and only helps inform the cardano node
+    -- on how to best handle the header and block.
+    byronHeader =
+        (snd $ List.head $ Byron.examples.exampleHeader)
+            { Byron.byronHeaderBlockSizeHint = Byron.fakeByronBlockSizeHint
+            }
